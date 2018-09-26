1cb6598885f9b4280e4399dcfe2633c86e50486b
gabrielmandelli <gabriel-mandelli@hotmail.com>
2018-09-26 00:38:00 -0300

    Add Timer and Formula
    - ajust drip-formula
    - ajust frequency-tomer

--- src/pages/drip-formula/drip-formula.ts
+++ src/pages/drip-formula/drip-formula.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormGroup, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'page-drip-formula',
@@ -7,8 +8,38 @@ import { NavController } from 'ionic-angular';
 })
 export class DripFormula {
 
-  constructor(public navCtrl: NavController) {
+  dripForm : FormGroup;
+  private volume: number;
+  private tempo: number;
+  utilizaMacrogota: boolean;
+  public qtdGotasPorMinuto: number;
 
+  constructor(public navCtrl: NavController) {
   }
 
+  ngOnInit(){
+    this.dripForm  = new FormGroup({
+      'volume': new FormControl(this.volume),
+      'tempo': new FormControl(this.tempo),
+      'utilizaMacrogota': new FormControl(this.utilizaMacrogota)
+    })
+  }
+
+  onSubmit(){
+    this.getDripFormula();
+  }
+
+  getDripFormula(){
+    let lbUtilizaMacrogota = this.dripForm.get("utilizaMacrogota").value
+    
+    let volume = this.dripForm.get("volume").value
+    let tempo = this.dripForm.get("tempo").value
+    let lMicrogot = ( volume / (tempo * 3));
+    
+    if (lbUtilizaMacrogota){
+      lMicrogot = lMicrogot * 3;
+    }
+
+    this.qtdGotasPorMinuto = Math.round(lMicrogot);
+  }
 }
