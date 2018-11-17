aa44655877a6fda617c654eb31124309b2d82902
gabrielmandelli <gabriel-mandelli@hotmail.com>
2018-11-16 23:37:29 -0200

    -- Ajust Gotejamento.

--- src/pages/frequency-setting/frequency-setting.ts
+++ src/pages/frequency-setting/frequency-setting.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormGroup, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'page-frequency-setting',
@@ -8,8 +9,49 @@ import { NavController } from 'ionic-angular';
 
 export class FrequencySetting {
 
-  constructor(public navCtrl: NavController) {
+  frequencySetting : FormGroup;
+  public qtdGotasPorMinuto : number;
 
+  timeFirstTap = 0 ;
+  timeSecondTap = 0;
+
+  timeInterval = 0;
+
+  public intervalVar;
+  public timer = 0;
+
+  constructor(public navCtrl: NavController) {
+    
   }
 
+  ngOnInit(){
+    this.frequencySetting  = new FormGroup({
+      'gotasPorMinuto': new FormControl()
+    })
+    this.qtdGotasPorMinuto = 0;
+  }
+  
+  onSubmit(){
+    
+    if ((this.timeFirstTap == 0) || (this.timeSecondTap == 0)){
+      this.startTimer();
+    }
+
+    this.timeSecondTap = this.timeFirstTap
+    this.timeFirstTap = this.timer
+
+    console.log(this.timeSecondTap +"   "+ this.timeFirstTap)
+
+    if (this.timeFirstTap > this.timeSecondTap){
+      this.timeInterval = this.timeFirstTap - this.timeSecondTap;
+      this.qtdGotasPorMinuto = Math.round((this.timeInterval / 60));
+      console.log(this.timeInterval);
+    }
+  }
+
+  startTimer(){
+    this.intervalVar = setInterval(function(){
+      this.timer++;
+    }.bind(this), 1)
+  }
 }
