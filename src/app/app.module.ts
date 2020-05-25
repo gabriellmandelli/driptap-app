23351cd6f497c248bcf8bff89ccd61d658c2870e
gabrielmandelli <gabriel-mandelli@hotmail.com>
2020-05-25 12:44:24 -0300

    Update path of pages from src to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { ContactPage } from '../pages/contact/contact';
-import { DripFormula } from '../pages/drip-formula/drip-formula';
-import { FrequencyTimer } from '../pages/frequency-timer/frequency-timer';
-import { FrequencySetting } from '../pages/frequency-setting/frequency-setting';
-import { TabsPage } from '../pages/tabs/tabs';
+import { ContactPage } from './pages/contact/contact';
+import { DripFormula } from './pages/drip-formula/drip-formula';
+import { FrequencyTimer } from './pages/frequency-timer/frequency-timer';
+import { FrequencySetting } from './pages/frequency-setting/frequency-setting';
+import { TabsPage } from './pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
